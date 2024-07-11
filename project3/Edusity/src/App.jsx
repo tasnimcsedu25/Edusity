1d0c80c9c028602e35b347f462881e00049e8d6c
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-07-11 20:37:29 +0600

    campus section done

--- project3/Edusity/src/App.jsx
+++ project3/Edusity/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title' 
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 const App=()=> {
   return (
     <div>
@@ -14,6 +15,8 @@ const App=()=> {
     <Programs/>
     <About/>
     <Title subTitle='Gallery' title='Campus Photos'/>
+    <Campus/>
+
   </div>
     
    
