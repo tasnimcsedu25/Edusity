21177d3e8caf219df60c7ec0a061cdd2a33aac1d
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-07-12 18:47:39 +0600

    final commit

--- project3/Edusity/src/App.jsx
+++ project3/Edusity/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -8,7 +8,9 @@ import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/Video player/VideoPlayer'
 const App=()=> {
+  const [playState,setPlayState]=useState(false);
   return (
     <div>
     <Navbar/>
@@ -16,7 +18,7 @@ const App=()=> {
   <div className="container">
     <Title subTitle='Our program' title='What we offer'/>
     <Programs/>
-    <About/>
+    <About setPlayState={setPlayState}/>
     <Title subTitle='Gallery' title='Campus Photos'/>
     <Campus/>
     <Title subTitle='TESTIMONIALS' title='What Student Says'/>
@@ -26,7 +28,7 @@ const App=()=> {
     <Footer/>
 
   </div>
-    
+    <VideoPlayer playState={playState} setPlayState={setPlayState}/>
    
     </div>
   )
--- project3/Edusity/src/Components/About/About.jsx
+++ project3/Edusity/src/Components/About/About.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './About.css' 
 import about_img from '../../assets/about.png'
 import play_icon from '../../assets/play-icon.png'
-const About = () => {
+const About = ({setPlayState}) => {
   return (
     <div className='about'>
         <div className="about-left">
             <img src={about_img}className='about-img'/>
-            <img src={play_icon} className='play-icon'/>
+            <img src={play_icon} className='play-icon' onClick={()=>{setPlayState(true)}}/>
         </div>
         <div className="about-right">
             <h3>About university</h3>
