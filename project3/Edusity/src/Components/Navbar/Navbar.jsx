e3b1d6e414319cf09edea50e6d2aa6f02c0f9c1d
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-07-12 13:01:40 +0600

    react scroll done

--- project3/Edusity/src/Components/Navbar/Navbar.jsx
+++ project3/Edusity/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import {Link, link} from 'react-scroll'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 const Navbar = () => {
@@ -13,12 +14,12 @@ const Navbar = () => {
       <img src={logo} className='logo'/>
       
     <ul>
-        <li>home</li>
-        <li>Program</li>
-        <li>About</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact us </button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500}className='btn'>Contact us </Link></li>
     </ul>
     </nav>
   )
